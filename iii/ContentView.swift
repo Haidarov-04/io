21cf6f5fe22496045617d3b4e5f945393f14159c
haidarov_n <nozimjonhaydarov6@gmail.com>
2025-02-03 16:41:34 +0500

    Initial Commit

---
+++ iii/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iii
+//
+//  Created by Haidarov N on 2/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iii/iiiApp.swift
@@ -0,0 +1,17 @@
+//
+//  iiiApp.swift
+//  iii
+//
+//  Created by Haidarov N on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct iiiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
